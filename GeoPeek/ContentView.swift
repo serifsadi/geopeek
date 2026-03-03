6a1006cf257712a4a13740b5d1e3c10f0a3622b2
Serif Sadi Senkule <serifsadi88@gmail.com>
2026-03-03 22:26:42 +0300

    Initial Commit

---
+++ GeoPeek/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeoPeek
+//
+//  Created by Şerif Şadi Şenkule on 28.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeoPeek/GeoPeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoPeekApp.swift
+//  GeoPeek
+//
+//  Created by Şerif Şadi Şenkule on 28.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct GeoPeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
